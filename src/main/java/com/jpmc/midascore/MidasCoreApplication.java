4ff153582d1b7221ec2c6fdf112d83dc8baa8110
Ashwani93333 <ashwanikumar93333@gmail.com>
2026-03-06 12:46:13 +0530

    Successfully Tested TaskOneTests

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -2,7 +2,9 @@ package com.jpmc.midascore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@EnableJpaRepositories(basePackages = "com.jpmc.midascore.repository")
 @SpringBootApplication
 public class MidasCoreApplication {
 
